32719e4a98b7f9527a737af723b42de2d92c0649
Alex Fuentes <alfupe@gmail.com>
2020-06-06 11:13:18 +0200

    ant design no se lleva bien por ahora con React.StrictMode

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import './i18n/i18n';
 import './assets/scss/main.scss';
 
-ReactDOM.render(
-  /*<React.StrictMode>*/
-    <App />
-  /*</React.StrictMode>*/,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
